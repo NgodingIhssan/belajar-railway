442d6d1e9b8f651c6fe8c27b064ed1f12dfd6183
Thaariq Ihsan <ihsanthaariq@gmail.com>
2025-05-28 10:57:32 +0700

    push

--- resources/views/tampil.blade.php
+++ resources/views/tampil.blade.php
@@ -3,14 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
-    <p>ini thoriq budak jelema hirup</p>
+    <p>
+        ini thaariq
+    </p>
     <td>
         <tr>
-            <a href="/tampil">pencet tampil ada hadiah</a>
+            <a href="/">balikk</a>
         </tr>
     </td>
+
 </body>
 </html>
